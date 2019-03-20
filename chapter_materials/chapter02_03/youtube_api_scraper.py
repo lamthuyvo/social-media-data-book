53b55f5cbc92433c6b49c05ce8e8d4213529ffa4
Lam Thuy Vo <lamthuyvo@gmail.com>
2019-03-20 14:42:54 -0400

    added functioning scripts

---
+++ chapter_materials/chapter02_03/youtube_api_scraper.py
@@ -0,0 +1,41 @@
+# this is where we specify which parts of python and which libraries we may need
+import json
+import csv
+
+import requests
+
+
+channel_id = 'UCJFp8uSYCjXOMnkUyb3CQ3Q'
+channel_id2 ='UCpko_-a4wgz2u_DgDgd9fqA'
+youtube_api_key  ='' # <-- your API key goes in between these two quotation marks!
+
+def makeCSV(page_id):
+    base = 'https://www.googleapis.com/youtube/v3/search?'
+    fields = '&part=snippet&channelId='
+    api_key = '&key=' + youtube_api_key
+    api_url = base + fields + channel_id + api_key
+
+    api_response = requests.get(api_url)
+    videos = json.loads(api_response.text.encode("ascii", "ignore")))
+
+
+    with open('%syoutube_videos.csv' % channel_id, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        csv_writer.writerow(['publishedAt',
+                            'title',
+                            'description',
+                            'thumbnailurl'])
+        has_another_page = True
+        while has_another_page:
+            for video in videos['items']:
+                video_data_row  = [ video['snippet']['publishedAt'], video['snippet']['title'], video['snippet']['description'], video['snippet']['thumbnails']['default']['url']]
+                csv_writer.writerow(video_data_row)
+            if 'nextPageToken' in videos.keys():
+                next_page_url = api_url + "&pageToken=" +videos['nextPageToken']
+                next_page_posts = requests.get(next_page_url)
+                videos = json.loads(next_page_posts.text)
+            else:
+                print('no more videos!')
+                has_another_page = False
+
+makeCSV(channel_id)
---
+++ chapter_materials/chapter04/ad_scraper.py
@@ -0,0 +1,34 @@
+import csv
+from bs4 import BeautifulSoup
+
+rows = []
+foldername = 'facebook-lamthuyvo'
+
+with open('%s/ads/advertisers_you've_interacted_with.html' % foldername) as page:
+    soup = BeautifulSoup(page,  'html.parser')
+    contents = soup.find('div', class_='_4t5n')
+    ad_list = contents.find_all( 'div' , class_='uiBoxWhite')
+
+    for item in ad_list:
+        advert = item.find('div', class_='_2let').get_text()
+        metadata = item.find('div', class_='_2lem').get_text()
+
+        row = { 'advert': advert,
+                'metadata': metadata
+              }
+        rows.append(row)
+
+
+# make a new csv into which we will write all the rows
+with open('%s-all-advertisers.csv' % foldername, 'w+') as csvfile:
+    # these are the header names:
+    fieldnames = ['advert', 'metadata']
+    # this creates your csv
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    # this writes in the first row, which are the headers
+    writer.writeheader()
+
+    # this loops through your rows (the array you set at the beginning and have updated throughtout)
+    for row in rows:
+        # this takes each row and writes it into your csv
+        writer.writerow(row)
